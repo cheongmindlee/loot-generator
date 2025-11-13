1de5f8aefe6349d4e52442a32c0ec7075adce6a2
David Lee <Cheongmin.gitlee@gmail.com>
2025-11-13 17:29:52 -0600

    Implemented the TreasureClassHashMap class which takes in all of the treasure class items and creates a hash map where the tresausre class is the key and the values are the items that could be dropped

--- src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
@@ -4,6 +4,7 @@ package edu.grinnell.csc207.lootgenerator;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 
 public class LootGenerator {
     /** The path to the dataset (either the small or large set). */
@@ -20,9 +21,20 @@ public class LootGenerator {
         //     System.out.println(monster);
         // }
 
+        //Delete at end
         // Make every element an Monser 
         // test choose a random monster and get its name, value, level and treasure class
-        System.out.println(monsters.getMonsters().get(5).getLevel());
+        //System.out.println(monsters.getMonsters().get(5).getLevel());
+
+        TreasureClassHashMap treasureHM = 
+            new TreasureClassHashMap("/Users/david/csc207/loot-generator/data/large/TreasureClassEx.txt");
+        HashMap<String, String[]> checkHM = treasureHM.getTreasureHM();
+        String[] check = checkHM.get("armo30");
+        for(String item: check){
+            System.out.println(item);
+        }
+
+
         // TOOD: Implement me!
     }
 }
