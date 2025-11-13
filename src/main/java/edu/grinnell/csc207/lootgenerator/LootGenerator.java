8ec274e812cce32ec9ab70a5efe1bd5305bbe294
David Lee <Cheongmin.gitlee@gmail.com>
2025-11-13 11:04:25 -0600

    Implemented MonstersList class which creates a list of Monster elements for each monster in the dataset

--- src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
@@ -1,11 +1,28 @@
 package edu.grinnell.csc207.lootgenerator;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class LootGenerator {
     /** The path to the dataset (either the small or large set). */
     private static final String DATA_SET = "data/small";
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("This program kills monsters and generates loot!");
+
+        //Get the data set and sort each monster into an index inside MonstersList class
+        MonstersList monsters = new MonstersList("/Users/david/csc207/loot-generator/data/large/monstats.txt");
+    
+        //Delete at the end
+        // for(String monster: monsters.getMonsters()){
+        //     System.out.println(monster);
+        // }
+
+        // Make every element an Monser 
+        // test choose a random monster and get its name, value, level and treasure class
+        System.out.println(monsters.getMonsters().get(5).getLevel());
         // TOOD: Implement me!
     }
 }
