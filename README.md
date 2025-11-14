66cb48d16e6d36c28bb9c3c300e51b00dbae123f
David Lee <Cheongmin.gitlee@gmail.com>
2025-11-13 18:49:34 -0600

    Implemented the armor class which takes in the armor txt and creates a hash map where the armor name is the key and the values are a list containing the min and max ac values. I also implemented the generateBaseStat function which takes a given equipment and returns a random ac value within the range of the armors ac.

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Authors: (_TODO: fill in names here!_)
 
 ## Resources
 
-*   (_TODO: fill in resources here!_)
-*   ...
-*   ...
+- https://www.baeldung.com/java-string-split-by-newline
+-     - Explained to me \\R instead of \n to parse by newlines
+- https://www.geeksforgeeks.org/java/arrays-copyof-in-java-with-examples/
+-     - Explained to me the Arrays.copyOf function to get a subarray
+- ...
 
 ## Revision Log
 
-*   (_TODO: fill in your changes to your project here!_)
+- (_TODO: fill in your changes to your project here!_)
--- src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
@@ -2,9 +2,8 @@ package edu.grinnell.csc207.lootgenerator;
 
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
 
 public class LootGenerator {
     /** The path to the dataset (either the small or large set). */
@@ -13,9 +12,16 @@ public class LootGenerator {
     public static void main(String[] args) throws IOException{
         System.out.println("This program kills monsters and generates loot!");
 
-        //Get the data set and sort each monster into an index inside MonstersList class
-        MonstersList monsters = new MonstersList("/Users/david/csc207/loot-generator/data/large/monstats.txt");
+
+        //Get the data set and put inside a MonstersList object
+        MonstersList monsters = new MonstersList("/Users/david/csc207/loot-generator/data/small/monstats.txt");
+
+        //Get data set of every treasure class and put them into TreasureClassHashMap object
+        TreasureClassHashMap treasureHM = 
+            new TreasureClassHashMap("/Users/david/csc207/loot-generator/data/small/TreasureClassEx.txt");
     
+        //Get data set of every armor item into am Armor object
+        Armor armorHM = new Armor("/Users/david/csc207/loot-generator/data/small/armor.txt");
         //Delete at the end
         // for(String monster: monsters.getMonsters()){
         //     System.out.println(monster);
@@ -26,15 +32,113 @@ public class LootGenerator {
         // test choose a random monster and get its name, value, level and treasure class
         //System.out.println(monsters.getMonsters().get(5).getLevel());
 
-        TreasureClassHashMap treasureHM = 
-            new TreasureClassHashMap("/Users/david/csc207/loot-generator/data/large/TreasureClassEx.txt");
-        HashMap<String, String[]> checkHM = treasureHM.getTreasureHM();
-        String[] check = checkHM.get("armo30");
-        for(String item: check){
-            System.out.println(item);
+        //Delete at end
+        // TreasureClassHashMap treasureHM = 
+        //     new TreasureClassHashMap("/Users/david/csc207/loot-generator/data/large/TreasureClassEx.txt");
+        // HashMap<String, String[]> checkHM = treasureHM.getTreasureHM();
+        // String[] check = checkHM.get("armo30");
+        // for(String item: check){
+        //     System.out.println(item);
+        // }
+
+        //Initialize scanner for user input
+        Scanner input = new Scanner(System.in);
+
+        //Set game state to true until user responds with n or N
+        boolean gameState = true;
+        //Program loop, keep running until user responds with n or N
+        while(gameState){
+            //Generate a monster to fight
+            Monster monster = pickMonster(monsters);
+            String monsterName = monster.getMonsterClass();
+            String monsterClass = monster.getTreasure();
+
+            //generate a base item
+            String baseItem = generateBaseItem(treasureHM, monsterClass);
+
+            //generate stat of base item
+            int itemStat = generateBaseStat(armorHM, baseItem);
+            //Give output to user
+            System.out.println("Fighting " + monsterName);
+            System.out.println("You have slain " + monsterName + "!");
+            System.out.println(monsterName + " dropped:\n");
+            System.out.println(baseItem);
+            System.out.println("Defense: " + itemStat);
+
+            //Once one round has been complete ask user if they wish to play again
+            while(true){
+                System.out.println("Fight again [y\\n]?");
+                
+                //Take in user input and end the game if they input n, continue if Y, for incorrect input keep prompting 
+                //  for a response
+                String response = input.nextLine();
+
+                if(response.equals("y") || response.equals("Y")){
+                    break;
+                } else if(response.equals("n") || response.equals("N")){
+                    gameState = false;
+                    break;
+                } else{
+                    continue;
+                }
+            }
+
         }
-
-
         // TOOD: Implement me!
+    }
+
+    /**
+     * Generates a random monster from the dataset
+     * @param monsters a MonsterList object
+     * @return a Monster object
+     */
+    public static Monster pickMonster(MonstersList monsters){
+        Random rand = new Random();
+        int size = monsters.getMonsters().size();
+        int randomInt = rand.nextInt(size);
+
+        return monsters.getMonster(randomInt);
+    }
+
+
+    /**
+     * Randomly selects an item from the treasureclass the corresponds to a monster, and returns it
+     * @param treasureHM A TreasureClassHashMap object
+     * @param treasureClass a String representation of a treasure class
+     * @return String representation of baseItem randomly selected
+     */
+    public static String generateBaseItem(TreasureClassHashMap treasureHM, String treasureClass){
+        Random rand = new Random();
+        int randomInt = rand.nextInt(3);
+        String[] items = treasureHM.get(treasureClass);
+
+        String baseItem = items[randomInt];
+
+        //Now check to see if this base item is itself another treasure class. If it isn't generate another item
+        if(treasureHM.contains(baseItem)){
+            return generateBaseItem(treasureHM, baseItem);
+        } else {
+            return baseItem;
+        }
+        
+    }
+
+    /**
+     * Returns a random integer between the min and max values of a piece of armor from
+     *  the armor class 
+     * @param armorHM an Armor object
+     * @param armor String reprsentation of armor
+     * @return a positive integer
+     */
+    public static int generateBaseStat(Armor armorHM, String armor){
+        //Take in the min and max values of the given armor
+        Integer[] minMax = armorHM.getArmorMinMax(armor);
+        int min = minMax[0];
+        int max = minMax[1];
+
+        //Generate a random stat between min and max
+        Random rand = new Random();
+        int randomInt = rand.nextInt(min, max) + 1;
+        return randomInt;
     }
 }
